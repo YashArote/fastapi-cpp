f122edf8890ddc5cc9f73614e4b96ffbdd0002fe
Yash <yasharote28@gmail.com>
2025-10-01 19:27:23 +0530

    new arrangement

--- core.cpp
+++ core.cpp
@@ -1,11 +1,5 @@
 #include <iostream>
-#include "httplib.h"
-#include "core/response.h"
-#include "core/request.h"
-#include "core/router.h"
-#include "core/macros.h"
-#include "core/validation.h"
-
+#include "fastapi_cpp.h"
 Router app;
 
 // ---------------- Models ----------------
@@ -34,27 +28,5 @@ Response update_user(int id, UserModel user) {
 int main() {
     APP_GET("/items/{item_id}", read_item, Path<int>);
     APP_POST("/users/{id}", update_user, Path<int>, Body<UserModel>);
-
-    httplib::Server svr;
-
-    svr.Get(R"(.*)", [](const httplib::Request &req, httplib::Response &res) {
-        Response app_res = app.handle_request("GET", req.path);
-        res.set_content(app_res.dump(), app_res.content_type);
-    });
-
-    svr.Post(R"(.*)", [](const httplib::Request &req, httplib::Response &res) {
-        std::optional<json> parsed;
-        try {
-            if (!req.body.empty()) parsed = json::parse(req.body);
-        } catch (...) {
-            res.status = 400;
-            res.set_content("{\"error\":\"Invalid JSON\"}", "application/json");
-            return;
-        }
-        Response app_res = app.handle_request("POST", req.path, parsed);
-        res.set_content(app_res.dump(), app_res.content_type);
-    });
-
-    std::cout << "Server running at http://localhost:9006\n";
-    svr.listen("127.0.0.1", 9006);
+    FastApiCpp::run(app, "127.0.0.1", 8080);    
 }
